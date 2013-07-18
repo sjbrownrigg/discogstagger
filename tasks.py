7cd61add2980f29a17f8e66d87a7b9d80be57d10
Markus M. May <mmay@javafreedom.org>
2013-07-18 20:41:46 +0200

    Add invoke to run tests easier

---
+++ tasks.py
@@ -0,0 +1,9 @@
+from invoke import task, run
+
+@task
+def clean():
+    run("rm -rf cover *.pyc discogstagger/*.pyc ext/*.pyc .coverage")
+
+@task('clean')
+def test():
+    run("nosetests --with-coverage --cover-erase --cover-branches --cover-html --cover-package=discogstagger --cover-min-percentage=75")
