0e1de454c6e20590bd612c676d14b183e8341304
Stewart <Stewart.Brownrigg@gmail.com>
2019-11-23 16:41:52 +0000

    Minor debug statement change

--- discogstagger/fileutils.py
+++ discogstagger/fileutils.py
@@ -71,7 +71,7 @@ class FileUtils(object):
                     audio_files.append(file)
             for dir in dirs:
                 if re.search('^(?i)(cd|disc)\s*\d+', dir):
-                    logger.info('Directory has cd/disc subdirectories')
+                    logger.debug('Directory has cd/disc subdirectories')
                     unwalk.append(dir)
                     d = Path(os.path.join(root, dir))
                     for file in d.iterdir():
